6d868d4b0aa39d7e30984e7fddd446bf0c0c212d
Enhex <enhex0@gmail.com>
2018-01-16 05:23:25 +0200

    improved string test

--- tests/string.cpp
+++ tests/string.cpp
@@ -6,29 +6,41 @@ using asio::ip::tcp;
 
 constexpr unsigned short port = 27000;
 
-void on_accept(tcp::socket& socket)
-{
-	std::cout << read_string(socket) << '\n';
-}
-
 int main()
 {
-	asio::io_context context;
-
-	tcp::acceptor acceptor(context, tcp::endpoint(tcp::v4(), port));
-
-	tcp::socket socket(context);
-	acceptor.async_accept(socket, [&](std::error_code ec) { on_accept(socket); });
-
-	auto read_future = std::async([&context] { context.run(); });
-
-	std::puts("writing string...");
+	try
 	{
+		asio::io_context context;
+
+		tcp::acceptor acceptor(context, tcp::endpoint(tcp::v4(), port));
+
 		tcp::socket socket(context);
-		const auto endpoint = tcp::endpoint(asio::ip::address_v4({127,0,0,1}), port);
-		socket.connect(endpoint);
-		
-		const std::string str = "hello world!";
-		write_string(socket, str);
+		tcp::socket connect_socket(context);
+
+		acceptor.async_accept(socket, [&](std::error_code ec)
+		{
+			const std::string str = "hello ";
+			write_string(connect_socket, str);
+
+			auto str1 = read_string(socket);
+			std::cout << str1 << '\n';
+
+			write_string(connect_socket, "world!");
+
+			auto str2 = read_string(socket);
+			std::cout << str2 << '\n';
+		});
+
+
+		auto fut1 = std::async([&context] { context.run(); });
+
+		std::puts("writing string...");
+
+		const auto endpoint = tcp::endpoint(asio::ip::address_v4({ 127,0,0,1 }), port);
+		connect_socket.connect(endpoint);
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
 	}
 }
