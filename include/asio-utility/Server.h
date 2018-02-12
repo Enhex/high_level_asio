fa0f1c76349646e5f46d6ecb931393cbcacf4ddd
Enhex <enhex0@gmail.com>
2018-02-12 14:33:58 +0200

    Server: pass socket by reference instead of moving it

--- include/asio-utility/Server.h
+++ include/asio-utility/Server.h
@@ -17,12 +17,13 @@ struct Server
 	tcp::socket socket;
 
 	// accept one new connection at a time asynchronously
+	//NOTE: move the socket in OnAccept to control its lifetime
 	template<typename OnAccept>
 	void accept(OnAccept&& on_accept)
 	{
 		socket = tcp::socket(acceptor.get_io_context()); // create a new socket in case the last one was moved
 		acceptor.async_accept(socket, [&, on_accept = std::move(on_accept)](std::error_code /*ec*/) {
-			on_accept(std::move(socket)); // move so the callback can keep the socket
+			on_accept(socket);
 			accept(std::move(on_accept));
 		});
 	}
