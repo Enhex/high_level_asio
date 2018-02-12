70a62347fca1a384e170b2e556f1a5d23f3bcd22
Enhex <enhex0@gmail.com>
2018-02-12 14:32:27 +0200

    fixed streambuf usage

--- include/asio-utility/string.h
+++ include/asio-utility/string.h
@@ -18,20 +18,14 @@ void write_string(SyncWriteStream& stream, const std::string& str)
 
 
 // read a std::string as a null-terminated string, with a re-usable asio::streambuf (make sure to clear it when needed)
+//NOTE: data beyond the delimiter may be read into the buffer, so you need to use it for subsequent reads.
 template<typename SyncReadStream>
 std::string read_string(SyncReadStream& stream, asio::streambuf& sb)
 {
 	const auto str_size = asio::read_until(stream, sb, '\0') - 1;
 	const auto bufs = sb.data();
+	sb.consume(str_size + 1);
 	return std::string(
 		asio::buffers_begin(bufs),
 		asio::buffers_begin(bufs) + str_size);
-}
-
-// read a std::string as a null-terminated string
-template<typename SyncReadStream>
-std::string read_string(SyncReadStream& stream)
-{
-	asio::streambuf sb;
-	return read_string(stream, sb);
 }
