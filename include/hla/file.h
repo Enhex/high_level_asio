5474ec36eb7c666d5fbd79114ea8eb1a17febbf4
Enhex <enhex0@gmail.com>
2018-02-16 00:17:54 +0200

    add `hla` namespce. use snake_case to match Asio naming.

--- include/hla/file.h
+++ include/hla/file.h
@@ -11,54 +11,57 @@
 #include <iostream>
 #include <vector>
 
-// write a file using size tag
-//TODO file size template parameter
-template<typename SyncWriteStream>
-void write_file(SyncWriteStream& stream, const std::string& path)
+namespace hla
 {
-	std::ifstream file(path, std::ios::binary);
+	// write a file using size tag
+	//TODO file size template parameter
+	template<typename SyncWriteStream>
+	void write_file(SyncWriteStream& stream, const std::string& path)
+	{
+		std::ifstream file(path, std::ios::binary);
 
-	std::vector<char> content(
-		(std::istreambuf_iterator<char>(file)),
-		std::istreambuf_iterator<char>());
+		std::vector<char> content(
+			(std::istreambuf_iterator<char>(file)),
+			std::istreambuf_iterator<char>());
 
-	// use fixed 32 bit unsigned int
-	const auto content_size = content.size();
-	if (content_size > std::numeric_limits<uint32_t>::max()) {
-		throw std::length_error("File size too big to be represented by uint32_t.");
+		// use fixed 32 bit unsigned int
+		const auto content_size = content.size();
+		if (content_size > std::numeric_limits<uint32_t>::max()) {
+			throw std::length_error("File size too big to be represented by uint32_t.");
+		}
+		const auto file_size = uint32_t(content_size);
+
+		asio::error_code error;
+
+		// write file size
+		asio::write(stream, asio::buffer(&file_size, sizeof(file_size)), error);
+		if (error)
+			std::cerr << error.message();
+
+		// write file
+		asio::write(stream, asio::buffer(content, file_size), error);
+		if (error)
+			std::cerr << error.message();
 	}
-	const auto file_size = uint32_t(content_size);
 
-	asio::error_code error;
-	
-	// write file size
-	asio::write(stream, asio::buffer(&file_size, sizeof(file_size)), error);
-	if (error)
-		std::cerr << error.message();
+	// read a file using size tag
+	template<typename SyncReadStream>
+	std::vector<std::byte> read_file(SyncReadStream& stream)
+	{
+		asio::error_code error;
 
-	// write file
-	asio::write(stream, asio::buffer(content, file_size), error);
-	if (error)
-		std::cerr << error.message();
-}
+		// read file size
+		uint32_t file_size = 0;
+		asio::read(stream, asio::buffer(&file_size, sizeof(file_size)), error);
+		if (error)
+			std::cerr << error.message();
 
-// read a file using size tag
-template<typename SyncReadStream>
-std::vector<std::byte> read_file(SyncReadStream& stream)
-{
-	asio::error_code error;
+		// read file
+		std::vector<std::byte> content(file_size);
+		asio::read(stream, asio::buffer(content, file_size), error);
+		if (error)
+			std::cerr << error.message();
 
-	// read file size
-	uint32_t file_size = 0;
-	asio::read(stream, asio::buffer(&file_size, sizeof(file_size)), error);
-	if (error)
-		std::cerr << error.message();
-
-	// read file
-	std::vector<std::byte> content(file_size);
-	asio::read(stream, asio::buffer(content, file_size), error);
-	if (error)
-		std::cerr << error.message();
-
-	return content;
+		return content;
+	}
 }
--- include/hla/utility.h
+++ include/hla/utility.h
@@ -4,32 +4,35 @@
 #include <thread>
 #include <vector>
 
-constexpr auto localhost_v4_bytes = asio::ip::address_v4::bytes_type{ 127,0,0,1 };
-const auto localhost_v4 = asio::ip::address_v4(localhost_v4_bytes);
-
-// create N threads and call asio::io_context::run() in them
-inline std::vector<std::thread> thread_pool_run(asio::io_context& context, unsigned threads_count)
+namespace hla
 {
-	std::vector<std::thread> threads;
+	constexpr auto localhost_v4_bytes = asio::ip::address_v4::bytes_type{ 127,0,0,1 };
+	const auto localhost_v4 = asio::ip::address_v4(localhost_v4_bytes);
 
-	for (; threads_count-- > 0;)
+	// create N threads and call asio::io_context::run() in them
+	inline std::vector<std::thread> thread_pool_run(asio::io_context& context, unsigned threads_count)
 	{
-		threads.emplace_back([&context] {
-			context.run();
-		});
+		std::vector<std::thread> threads;
+
+		for (; threads_count-- > 0;)
+		{
+			threads.emplace_back([&context] {
+				context.run();
+			});
+		}
+
+		return std::move(threads);
 	}
 
-	return std::move(threads);
-}
+	// create threads according to the number of available cores and call asio::io_context::run() in them
+	inline std::vector<std::thread> thread_pool_run(asio::io_context& context)
+	{
+		return thread_pool_run(context, std::thread::hardware_concurrency());
+	}
 
-// create threads according to the number of available cores and call asio::io_context::run() in them
-inline std::vector<std::thread> thread_pool_run(asio::io_context& context)
-{
-	return thread_pool_run(context, std::thread::hardware_concurrency());
-}
-
-inline void join_threads(std::vector<std::thread>& threads)
-{
-	for (auto& thread : threads)
-		thread.join();
+	inline void join_threads(std::vector<std::thread>& threads)
+	{
+		for (auto& thread : threads)
+			thread.join();
+	}
 }
